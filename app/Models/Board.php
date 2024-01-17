a0c314e3804935951981b4052be602c891d74d12
Joseph Kerkhof <joseph@kerkhof.dev>
2024-01-16 22:55:25 -0600

    fix: correct PSR-12 violations

--- app/Models/Board.php
+++ app/Models/Board.php
@@ -35,7 +35,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  */
 class Board extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
